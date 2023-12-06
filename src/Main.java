54bb76a1c3c25c77582410dab5bab35e17238d49
Gosha Petrov <gosha_petrov20@rambler.ru>
2023-12-06 21:50:44 +0300

    Project binary search numbers

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] prices = { 13, 17, 19, 25, 25, 25, 25, 25, 25, 27, 30};
+
+        System.out.println("Для 31: " + countMore(prices, 31)); // 0
+        System.out.println("Для 26: " + countMore(prices, 26)); // 2
+        System.out.println("Для 25: " + countMore(prices, 25)); // 2
+        System.out.println("Для 20: " + countMore(prices, 20)); // 8
+    }
+
+    public static int countMore(int[] prices, int money) {
+        if (prices[0] > money) {
+            return prices.length; // все недоступны
+        }
+
+        if (prices[prices.length - 1] <= money) {
+            return 0; // все доступны
+        }
+
+        int left = 0;
+        int right = prices.length - 1;
+        while (left < right) {
+            int middle = (left + right) / 2;
+            if (prices[middle] <= money) {
+                left = middle;
+            } else if (prices[middle] > money) {
+                if (prices[middle - 1] > money) {
+                    right = middle;
+                } else {
+                    return prices.length - middle;
+                }
+            }
+        }
+        return -1;
+    }
+}
+
